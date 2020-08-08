ce8b4aebfba2918757b787666b5ec43e00801725
pramidatumma <pramidat@thoughtworks.com>
2020-08-08 14:12:18 +0530

    Exception handling using wrapper lambdas

--- src/lambdas/ExceptionHandling.java
+++ src/lambdas/ExceptionHandling.java
@@ -6,19 +6,24 @@ public class ExceptionHandling {
     public static void main(String[] args) {
         int [] someNumbers = {1,2,3,4,5};
         int key = 0;
-        process(someNumbers, key, (p,k)-> {
-            try {
-                System.out.println(p / k);
-            }
-            catch (ArithmeticException e) {
-                System.out.println("ArithmeticException occured");
-            }
-        });
+        process(someNumbers, key, wrapperLambda((p,k)->System.out.println(p / k)));
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> biConsumer) {
         for(int i: someNumbers) {
             biConsumer.accept(i, key);
         }
+    }
+
+    private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> biConsumer) {
+        return (p,k) -> {
+//            System.out.println("Executing from wrapper");
+            try {
+                biConsumer.accept(p, k);
+            }
+            catch (ArithmeticException e) {
+                System.out.println("Exception occured");
+            }
+        };
     }
 }

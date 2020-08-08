9898c584c0311a2d7d5690a2c4ebf0be1178f462
pramidatumma <pramidat@thoughtworks.com>
2020-08-08 14:26:32 +0530

    Closures

---
+++ src/lambdas/ClosuresExample.java
@@ -0,0 +1,19 @@
+package lambdas;
+
+import java.util.function.Consumer;
+
+public class ClosuresExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        doProcess(a, k->System.out.println(k+b)); //considers the b and expects it to be finalr
+    }
+
+    public static void doProcess(int i, Consumer<Integer> consumer) {
+        consumer.accept(i);
+    }
+}
+
+interface Process {
+    void process (int i);
+}
